1aa3fc42ccdd69259f99da87a67be1403df0de5e
Neverlandwalker <Blonnyya@gmail.com>
2023-02-03 09:14:51 +0200

    commit pre last commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
 
+const galleryRoot = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+galleryRoot.insertAdjacentHTML('beforeend', cardsMarkup);
+
+
+function createGalleryCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+
+        return `
+        <a class="gallery__item" 
+        href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" 
+  />
+</a>
+        `;
+    })
+        .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: 'bottom',
+
+
+});
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+    
+const iframe = document.querySelector('iframe');
+const player = new Player('vimeo-player');
+const savedTimer = localStorage.getItem('videoplayer-current-time') || 0;
+console.log(savedTimer);
+player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds)
+    
+};
+player.setCurrentTime(savedTimer)
